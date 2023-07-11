e89af17278c6e4476087fdad4faf02062ef301af
joshuashea <joshshea44@gmail.com>
2023-07-11 13:29:44 -0700

    modified index & js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let q1 = prompt("Did Joel make the right decision at the end of The Last of Us (video game)?");
+let q2 = prompt("If a bar of soup falls on the floor, is it dirty?");
+let q3 = prompt("");
+let q4 = prompt("");
+let q5 = prompt("");
