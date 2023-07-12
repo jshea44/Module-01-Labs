840e2bafaaff81d8b4b55dcd04545c66ebf867e4
joshuashea <joshshea44@gmail.com>
2023-07-12 13:46:07 -0700

    modified js

--- js/app.js
+++ js/app.js
@@ -4,54 +4,47 @@ let userName = prompt("What is your name?");
 alert(`Welcome ${userName} to my website! I know this is sudden, but get ready because I'm about to ask you a series of questions.`);
 
 
-let q1 = prompt("Have you ever played a sport? (yes/no or y/n)").toLowerCase();
-let q2 = prompt("If a bar of soap falls on the floor, is it dirty? (yes/no or y/n)").toLowerCase();
-let q3 = prompt("Can you speak another language? (yes/no or y/n)").toLowerCase();
-let q4 = prompt("Do you enjoy coding? (yes/no or y/n)").toLowerCase();
-let q5 = prompt("Do even lift? (yes/no or y/n)").toLowerCase();
+// 5 questions
+const questions = [
+  "Have you ever played a sport? (yes/no or y/n)",
+  "If a bar of soap falls on the floor, is it dirty? (yes/no or y/n)",
+  "Can you speak another language? (yes/no or y/n)",
+  "Do you enjoy coding? (yes/no or y/n)",
+  "Do even lift? (yes/no or y/n)",
+];
 
-if (q1 === 'yes' || q1 === 'y') {
-  // console.log("Nice! I'm a bit of an underwater basket weaver myself.");
-  alert("Nice! I'm a bit of an underwater basket weaver myself.");
-} else if (q1 === 'no' || q1 === 'n') {
-  // console.log("That alright! It's never too late to pick one up.");
-  alert("That alright! It's never too late to pick one up.");
+for (let i = 0; i < questions.length; i++) {
+
+  let userChoice = prompt(questions[i]);
+
+  const yesResponses = [
+    "Nice! I'm a bit of an underwater basket weaver myself.",
+    "How can that be? You can't clean it, it's already soap.",
+    "Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.",
+    "That was a trick question. Of course your answer is going to be yes.",
+    "Been looking for a gym partner. See you 5:00am."
+  ];
+
+  const noResponses = [
+    "That alright! It's never too late to pick one up.",
+    "I agree. It's not like you can wash it, right? Rinse it off, maybe?",
+    "That's alright. One language is already hard enough isn't it?",
+    "Uncaught ReferenceError: " + userChoice + " is not defined.",
+    "Coding takes up more than enough time, doesn't it?"
+  ];
+  
+  if (userChoice == "yes" || userChoice == "y") {
+    // console.log(yesResponses[i]);
+    alert(yesResponses[i]);
+  } else if (userChoice == "no" || userChoice == "n") {
+    // console.log(noResponses[i]);
+    alert(noResponses[i]);
+  }
+
 }
 
-if (q2 === 'yes' || q2 === 'y') {
-  // console.log("How can that be? You can't clean it, it's already soap.");
-  alert("How can that be? You can't clean it, it's already soap.");
-} else if (q2 === 'no' || q2 === 'n') {
-  // console.log("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
-  alert("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
-}
 
-if (q3 === 'yes' || q3 === 'y') {
-  // console.log("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
-  alert("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
-} else if (q3 === 'no' || q3 === 'n') {
-  // console.log("That's alright. One language is already hard enough isn't it?");
-  alert("That's alright. One language is already hard enough isn't it?");
-}
+//number guessing game
+userNum = prompt(`Hey ${userName}, thank you so much for answering my questions! We aren't done yet. Now let's play a little number guessing game. 
+Try to guess a number between 1 and 10. You have 4 guesses. Good luck! `);
 
-if (q4 === 'yes' || q4 === 'y') {
-  // console.log("That was a trick question. Of course your answer is going to be yes.");
-  alert("That was a trick question. Of course your answer is going to be yes.");
-} else if (q4 === 'no' || q4 === 'n') {
-  // console.log("Uncaught ReferenceError: " + q4 + " is not defined.");
-  alert("Uncaught ReferenceError: " + q4 + " is not defined.");
-}
-
-if (q5 === 'yes' || q5 === 'y') {
-  // console.log("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
-  alert("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
-} else if (q5 === 'no' || q5 === 'n') {
-  // console.log("Coding takes up more than enough time, doesn't it?");
-  alert("Coding takes up more than enough time, doesn't it?");
-}
-
-if (q1 && q2 && q3 && q4 && q5) {
-  alert(`Hey ${userName}, thank you so much for answering my questions!`);
-} else {
-  alert(`Hey ${userName}, thank you so much for NOT answering any of my questions!`);
-}
