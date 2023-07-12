4978d42722126e8b0447f488d919dbb849a5b679
joshuashea <joshshea44@gmail.com>
2023-07-11 19:43:53 -0700

    modified css, index, and js

--- js/app.js
+++ js/app.js
@@ -1,57 +1,57 @@
 'use strict'
 
-let userName = prompt("What is your name?");
-alert(`Welcome ${userName} to my website! I know this is sudden, but get ready because I'm about to ask you a series of questions.`);
+// let userName = prompt("What is your name?");
+// alert(`Welcome ${userName} to my website! I know this is sudden, but get ready because I'm about to ask you a series of questions.`);
 
 
-let q1 = prompt("Have you ever played a sport? (yes/no or y/n)").toLowerCase();
-let q2 = prompt("If a bar of soap falls on the floor, is it dirty? (yes/no or y/n)").toLowerCase();
-let q3 = prompt("Can you speak another language? (yes/no or y/n)").toLowerCase();
-let q4 = prompt("Do you enjoy coding? (yes/no or y/n)").toLowerCase();
-let q5 = prompt("Do even lift? (yes/no or y/n)").toLowerCase();
+// let q1 = prompt("Have you ever played a sport? (yes/no or y/n)").toLowerCase();
+// let q2 = prompt("If a bar of soap falls on the floor, is it dirty? (yes/no or y/n)").toLowerCase();
+// let q3 = prompt("Can you speak another language? (yes/no or y/n)").toLowerCase();
+// let q4 = prompt("Do you enjoy coding? (yes/no or y/n)").toLowerCase();
+// let q5 = prompt("Do even lift? (yes/no or y/n)").toLowerCase();
 
-if (q1 === 'yes' || q1 === 'y') {
-  // console.log("Nice! I'm a bit of an underwater basket weaver myself.");
-  alert("Nice! I'm a bit of an underwater basket weaver myself.");
-} else if (q1 === 'no' || q1 === 'n') {
-  // console.log("That alright! It's never too late to pick one up.");
-  alert("That alright! It's never too late to pick one up.");
-}
+// if (q1 === 'yes' || q1 === 'y') {
+//   // console.log("Nice! I'm a bit of an underwater basket weaver myself.");
+//   alert("Nice! I'm a bit of an underwater basket weaver myself.");
+// } else if (q1 === 'no' || q1 === 'n') {
+//   // console.log("That alright! It's never too late to pick one up.");
+//   alert("That alright! It's never too late to pick one up.");
+// }
 
-if (q2 === 'yes' || q2 === 'y') {
-  // console.log("How can that be? You can't clean it, it's already soap.");
-  alert("How can that be? You can't clean it, it's already soap.");
-} else if (q2 === 'no' || q2 === 'n') {
-  // console.log("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
-  alert("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
-}
+// if (q2 === 'yes' || q2 === 'y') {
+//   // console.log("How can that be? You can't clean it, it's already soap.");
+//   alert("How can that be? You can't clean it, it's already soap.");
+// } else if (q2 === 'no' || q2 === 'n') {
+//   // console.log("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
+//   alert("I agree. It's not like you can wash it, right? Rinse it off, maybe?");
+// }
 
-if (q3 === 'yes' || q3 === 'y') {
-  // console.log("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
-  alert("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
-} else if (q3 === 'no' || q3 === 'n') {
-  // console.log("That's alright. One language is already hard enough isn't it?");
-  alert("That's alright. One language is already hard enough isn't it?");
-}
+// if (q3 === 'yes' || q3 === 'y') {
+//   // console.log("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
+//   alert("Fantastic! Fun fact: Only about 20% of people in the United States speak more than one language.");
+// } else if (q3 === 'no' || q3 === 'n') {
+//   // console.log("That's alright. One language is already hard enough isn't it?");
+//   alert("That's alright. One language is already hard enough isn't it?");
+// }
 
-if (q4 === 'yes' || q4 === 'y') {
-  // console.log("That was a trick question. Of course your answer is going to be yes.");
-  alert("That was a trick question. Of course your answer is going to be yes.");
-} else if (q4 === 'no' || q4 === 'n') {
-  // console.log("Uncaught ReferenceError: " + q4 + " is not defined.");
-  alert("Uncaught ReferenceError: " + q4 + " is not defined.");
-}
+// if (q4 === 'yes' || q4 === 'y') {
+//   // console.log("That was a trick question. Of course your answer is going to be yes.");
+//   alert("That was a trick question. Of course your answer is going to be yes.");
+// } else if (q4 === 'no' || q4 === 'n') {
+//   // console.log("Uncaught ReferenceError: " + q4 + " is not defined.");
+//   alert("Uncaught ReferenceError: " + q4 + " is not defined.");
+// }
 
-if (q5 === 'yes' || q5 === 'y') {
-  // console.log("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
-  alert("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
-} else if (q5 === 'no' || q5 === 'n') {
-  // console.log("Coding takes up more than enough time, doesn't it?");
-  alert("Coding takes up more than enough time, doesn't it?");
-}
+// if (q5 === 'yes' || q5 === 'y') {
+//   // console.log("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
+//   alert("Been looking for a gym partner. See you 5:00am.");alert("Been looking for a gym partner. See you 5:00am.");
+// } else if (q5 === 'no' || q5 === 'n') {
+//   // console.log("Coding takes up more than enough time, doesn't it?");
+//   alert("Coding takes up more than enough time, doesn't it?");
+// }
 
-if (q1 && q2 && q3 && q4 && q5) {
-  alert(`Hey ${userName}, thank you so much for answering my questions!`);
-} else {
-  alert(`Hey ${userName}, thank you so much for NOT answering any of my questions!`);
-}
+// if (q1 && q2 && q3 && q4 && q5) {
+//   alert(`Hey ${userName}, thank you so much for answering my questions!`);
+// } else {
+//   alert(`Hey ${userName}, thank you so much for NOT answering any of my questions!`);
+// }
